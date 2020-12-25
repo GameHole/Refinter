956b463f5f303b8dc3ca288969a4cfbec0b20f33
GameHole <741419087@qq.com>
2020-12-25 14:37:01 +0800

    修改BUG

---
+++ Assets/z_Test/TestA.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Refinter;
+namespace Default
+{
+    interface BBB : IInterface
+    {
+
+    }
+    class TTT: BBB
+    {
+
+    }
+    class TTT1 : BBB
+    {
+
+    }
+    [Important(2)]
+    class TTT2 : BBB
+    {
+
+    }
+    [Important(1)]
+    public class TestA:MonoBehaviour, BBB
+    {
+
+	}
+}

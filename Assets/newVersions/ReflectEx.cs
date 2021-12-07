2b25804e400717319af53bd944669bc00102d749
GameHole <741419087@qq.com>
2021-12-07 18:37:49 +0800

    发现bug

---
+++ Assets/newVersions/ReflectEx.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Reflection;
+namespace Refinter
+{
+    [AttributeUsage( AttributeTargets.Class, AllowMultiple =false,Inherited =false)]
+    public class ImportantAttribute : Attribute
+    {
+        internal int index;
+        public ImportantAttribute(int index)
+        {
+            this.index = index;
+        }
+    }
+    public static class ReflectEx
+    {
+        static Assembly[] _workAssemblies;
+        public static Assembly[] workAssemblies
+        {
+            get
+            {
+                if (_workAssemblies == null)
+                    LoadCSharp();
+                return _workAssemblies;
+            }
+        }
+        public static void Inject(object obj, object inter)
+        {
+            if (inter == null) return;
+            var itrType = inter.GetType();
+            foreach (var item in obj.GetType().GetFields(BindingFlags.Instance| BindingFlags.Public| BindingFlags.NonPublic))
+            {
+                if (item.FieldType.IsInterface && typeof(IInterface).IsAssignableFrom(item.FieldType) && item.FieldType.IsAssignableFrom(itrType))
+                {
+                    item.SetValue(obj, inter);
+                }
+            }
+
+        }
+        public static T Instance<T>()where T:class,IInterface
+        {
+            return Instance(typeof(T)) as T;
+        }
+        public static int GetIndex(Type type)
+        {
+            var att = type.GetCustomAttribute<ImportantAttribute>();
+            if (att != null)
+                return att.index;
+            return 0;
+        }
+        public static object Instance(Type type)
+        {
+            var find = FindImpl(type);
+            find.Sort((a,b) =>
+            {
+                return Compare(b, a);// GetIndex(b) - GetIndex(a);
+            });
+            //foreach (var item in find)
+            //{
+            //    Debug.Log($"find::{item}");
+            //}
+            for (int i = 0; i < find.Count; i++)
+            {
+                var f = find[i];
+                if (find[i].IsSubclassOf(typeof(MonoBehaviour)))
+                {
+                    var m = FindIMonoImpl(f);
+                    if (m != null)
+                        return m;
+                }
+                else
+                {
+                    return Activator.CreateInstance(f);
+                }
+            }
+            
+            return null;
+        }
+        public static int Compare(Type a, Type b)
+        {
+            return GetIndex(a) - GetIndex(b);
+        }
+        public static object FindIMonoImpl(Type type)
+        {
+            var f = Resources.FindObjectsOfTypeAll(type);
+            foreach (var item in f)
+            {
+                if ((item as MonoBehaviour).gameObject.scene.IsValid())
+                    return item;
+            }
+            return null;
+        }
+        public static List<Type> FindImpl(Type inter)
+        {
+            List<Type> finds = new List<Type>();
+            foreach (var assembly in workAssemblies)
+            {
+                foreach (var item in assembly.GetTypes())
+                {
+                    if (isIgnore(item)) continue;
+                    if (inter.IsAssignableFrom(item))
+                        finds.Add(item);
+                }
+            }
+            return finds;
+        }
+        static void LoadCSharp()
+        {
+            List<Assembly> assemblies = new List<Assembly>();
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                if (isIgnoredAssembliy(assembly)) continue;
+                assemblies.Add(assembly);
+            }
+            _workAssemblies = assemblies.ToArray();
+        }
+        static string[] ignoreStr = new string[]
+        {
+            "mscorlib","UnityEngine","UnityEngine.","UnityEditor","UnityEditor.","Newtonsoft.","SyntaxTree.","ExCSS.","Microsoft.","Unity.","System","System.","Mono.","nunit.","netstandard"
+        };
+        static bool isIgnoredAssembliy(Assembly assembly)
+        {
+            for (int i = 0; i < ignoreStr.Length; i++)
+            {
+                if (assembly.FullName.Contains(ignoreStr[i]))
+                    return true;
+            }
+            return false;
+        }
+        static bool isIgnore(Type type)
+        {
+            return type.IsInterface || type.IsAbstract || type.IsGenericType;
+        }
+    }
+}
---
+++ Assets/newVersions/Reflection.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Refinter
+{
+    public class Reflection : MonoBehaviour
+    {
+        internal static readonly Dictionary<Type, IInterface> interfaces = new Dictionary<Type, IInterface>();
+        public static Dictionary<Type, IInterface> Interfaces => interfaces;
+        static Queue<IInitializable> initializables = new Queue<IInitializable>();
+        static HashSet<Type> injected = new HashSet<Type>();
+        static HashSet<MonoBehaviour> monoInjected = new HashSet<MonoBehaviour>();
+        public bool debug;
+        public bool isInjectScene = true;
+        void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+            InjectSence();
+            if (isInjectScene)
+            {
+                SceneManager.sceneLoaded += (a, b) =>
+                {
+                    InjectSence();
+                };
+            }
+        }
+        public static T Get<T>()where T: IInterface
+        {
+            interfaces.TryGetValue(typeof(T), out var v);
+            return (T)v;
+        }
+        public static void Set<T>(IInterface inst)where T : IInterface
+        {
+            if (!typeof(T).IsInterface)
+                throw new ArgumentException($"type = {typeof(T)} is not a interface");
+            if (inst == null)
+                throw new NullReferenceException($"inst is null");
+            if(!interfaces.ContainsKey(typeof(T)))
+            {
+                throw new ArgumentException($"type = {typeof(T)} is not found in interface list");
+            }
+            interfaces[typeof(T)] = inst;
+            ReInjectInterface(typeof(T), inst);
+        }
+        void InjectSence()
+        {
+            Stopwatch stopwatch = null;
+            if (debug)
+            {
+                stopwatch = new Stopwatch();
+                stopwatch.Start();
+            }
+            ClearNullObj();
+            foreach (var assembly in ReflectEx.workAssemblies)
+            {
+                foreach (var item in assembly.GetTypes())
+                {
+                    if (item == typeof(IInterface) || interfaces.ContainsKey(item)) continue;
+                    if (typeof(IInterface).IsAssignableFrom(item) && item.IsInterface)
+                    {
+                        var instence = Instance(item);
+                        //if (instence == null)
+                        //{
+                        //    instence = ReflectEx.Instance(item) as IInterface;
+                        //    var init = (instence as IInitializable);
+                        //    if (init != null)
+                        //    {
+                        //        if (!initializables.Contains(init))
+                        //        {
+                        //            //UnityEngine.Debug.Log($"find init {init}");
+                        //            initializables.Enqueue(init);
+                        //        }
+                        //    }
+                        //}
+                        //UnityEngine.Debug.Log($"{item},{instence}");
+                        if (instence != null)
+                        {
+                            interfaces.Add(item, instence);
+                        }
+                    }
+                }
+            }
+          
+            
+            foreach (var obj in interfaces)
+            {
+                if (injected.Contains(obj.Key)) continue;
+                injected.Add(obj.Key);
+                foreach (var inter in interfaces.Values)
+                {
+                    ReflectEx.Inject(obj.Value, inter);
+                }
+            }
+            if (isInjectScene)
+            {
+                foreach (var obj in Resources.FindObjectsOfTypeAll<MonoBehaviour>())
+                {
+                    if (!obj.gameObject.scene.IsValid() || obj.GetType().FullName.Contains("UnityEngine")) continue;
+                    if (monoInjected.Contains(obj)) continue;
+                    monoInjected.Add(obj);
+                    Inject(obj);
+                    //Debug.Log(obj);
+                }
+            }
+            //foreach (var item in initializables)
+            //{
+            //    item.Initialize();
+            //}
+            //initializables.Clear();
+            while (initializables.Count > 0)
+            {
+                initializables.Dequeue().Initialize();
+            }
+
+            if (debug)
+            {
+                stopwatch.Stop();
+                UnityEngine.Debug.Log(stopwatch.Elapsed);
+            }
+        }
+        static void ReInjectInterface(Type type,IInterface inter)
+        {
+            foreach (var item in injected)
+            {
+                ReflectEx.Inject(interfaces[item], inter);
+            }
+            foreach (var item in monoInjected)
+            {
+                ReflectEx.Inject(item, inter);
+            }
+        }
+        public static IInterface Instance(Type type)
+        {
+            var instence = FindInterface(type);
+            if (instence == null)
+            {
+                instence = ReflectEx.Instance(type) as IInterface;
+                var init = (instence as IInitializable);
+                if (init != null)
+                {
+                    initializables.Enqueue(init);
+                }
+            }
+            return instence;
+        }
+        public static void Inject(MonoBehaviour mono)
+        {
+            foreach (var item in interfaces.Values)
+            {
+                ReflectEx.Inject(mono, item);
+            }
+        }
+        static IInterface FindInterface(Type type)
+        {
+            foreach (var item in interfaces.Values)
+            {
+                if (type.IsAssignableFrom(item.GetType()))
+                {
+                    if (ReflectEx.Compare(type, item.GetType()) <= 0)
+                        return item;
+                }
+            }
+            return null;
+        }
+        void ClearNullObj()
+        {
+            List<Type> removed = new List<Type>();
+            foreach (var item in interfaces.Keys)
+            {   
+                var v = interfaces[item];
+                //由于mono destroy后并不会立即置为null，故需要使用is判断是不是mono脚本
+                if (v is MonoBehaviour)
+                {
+                    //Debug.Log($"contain::key::{item},v::{v},isnull::{v == null}");
+                    //而 mono destroy 后使用as转换得到的结果却是null，因此转换后判断即可知道脚本是否被destroy
+                    if ((v as MonoBehaviour)==null)
+                    {
+                        removed.Add(item);
+                    }
+                }
+            }
+            foreach (var item in removed)
+            {
+                interfaces.Remove(item);
+            }
+            HashSet<MonoBehaviour> injected = new HashSet<MonoBehaviour>();
+            foreach (var item in monoInjected)
+            {
+                if (item)
+                {
+                    injected.Add(item);
+                }
+            }
+            monoInjected = injected;
+        }
+    }
+
+}
